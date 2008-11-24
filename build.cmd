ac38faa82ebf8fce35ade229c43788a944e03377
Atif Aziz <code@raboof.com>
2008-11-24 13:37:27 +0000

    Updated build and clean scripts to specifically use MSBUILD from .NET Framework 3.5 in order to require minimal environmental setup.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 pushd "%~dp0"
-for %%i in (Debug Release) do msbuild /p:Configuration=%%i src\NCrontab.sln
+for %%i in (Debug Release) do "%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild" /p:Configuration=%%i src\NCrontab.sln
 popd
