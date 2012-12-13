f2c0597b8cab24be28df1b91d0809bbc45a81b71
Atif Aziz <code@raboof.com>
2012-12-13 19:22:48 +0100

    Build batch script improvements

--- build.cmd
+++ build.cmd
@@ -1,4 +1,16 @@
 @echo off
 setlocal
 pushd "%~dp0"
-for %%i in (Debug Release) do "%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild" /p:Configuration=%%i src\NCrontab.sln
+call :main %*
+popd
+goto :EOF
+
+:main
+set MSBUILDEXE=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+if not exist "%MSBUILDEXE%" (
+    echo The .NET Framework 4.0 does not appear to be installed on this 
+    echo machine, which is required to build the solution.
+    exit /b 1
+)
+set EnableNuGetPackageRestore=true
+for %%s in (src\*.sln) do for %%c in (debug release) do "%MSBUILDEXE%" %%s /p:Configuration=%%c /v:m %*
