6ef179b01e4080b252804c23bb90d480cdf541cc
Atif Aziz <code@raboof.com>
2015-08-18 18:22:34 +0200

    Moving VS solution file to the root

--- build.cmd
+++ build.cmd
@@ -17,5 +17,5 @@ call :build Debug && call :build Release
 goto :EOF
 
 :build
-"%MSBUILDEXE%" src\NCrontab.sln /p:Configuration=%1 /v:m %2 %3 %4 %5 %6 %7 %8 %9
+"%MSBUILDEXE%" NCrontab.sln /p:Configuration=%1 /v:m %2 %3 %4 %5 %6 %7 %8 %9
 goto :EOF
