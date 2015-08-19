2383fd72c58bb5f3ab0a9ad228e215c53ce7f8ac
Atif Aziz <code@raboof.com>
2015-08-19 17:54:06 +0200

    NCrontabViewer GUI cosmetic improvements (font + layout)

--- NCrontabViewer/MainForm.Designer.cs
+++ NCrontabViewer/MainForm.Designer.cs
@@ -51,7 +51,7 @@ namespace NCrontabViewer
             this._startTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this._startTimePicker.Location = new System.Drawing.Point(65, 16);
             this._startTimePicker.Name = "_startTimePicker";
-            this._startTimePicker.Size = new System.Drawing.Size(166, 23);
+            this._startTimePicker.Size = new System.Drawing.Size(166, 27);
             this._startTimePicker.TabIndex = 0;
             this._startTimePicker.ValueChanged += new System.EventHandler(this.CronBox_Changed);
             // 
@@ -61,7 +61,7 @@ namespace NCrontabViewer
             this._endTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this._endTimePicker.Location = new System.Drawing.Point(298, 16);
             this._endTimePicker.Name = "_endTimePicker";
-            this._endTimePicker.Size = new System.Drawing.Size(166, 23);
+            this._endTimePicker.Size = new System.Drawing.Size(166, 27);
             this._endTimePicker.TabIndex = 1;
             this._endTimePicker.ValueChanged += new System.EventHandler(this.CronBox_Changed);
             // 
@@ -73,11 +73,11 @@ namespace NCrontabViewer
             // 
             // _resultBox
             // 
-            this._resultBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this._resultBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this._resultBox.BackColor = System.Drawing.SystemColors.Control;
-            this._resultBox.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._resultBox.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._resultBox.Location = new System.Drawing.Point(16, 88);
             this._resultBox.Name = "_resultBox";
             this._resultBox.ReadOnly = true;
@@ -88,11 +88,11 @@ namespace NCrontabViewer
             // 
             // _cronBox
             // 
-            this._cronBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this._cronBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this._cronBox.Location = new System.Drawing.Point(143, 48);
             this._cronBox.Name = "_cronBox";
-            this._cronBox.Size = new System.Drawing.Size(323, 23);
+            this._cronBox.Size = new System.Drawing.Size(323, 27);
             this._cronBox.TabIndex = 2;
             this._cronBox.Text = "* * * * *";
             this._cronBox.TextChanged += new System.EventHandler(this.CronBox_Changed);
@@ -102,7 +102,7 @@ namespace NCrontabViewer
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(16, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.Size = new System.Drawing.Size(43, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "&Start:";
             // 
@@ -111,7 +111,7 @@ namespace NCrontabViewer
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(255, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.Size = new System.Drawing.Size(37, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "&End:";
             // 
@@ -120,7 +120,7 @@ namespace NCrontabViewer
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(16, 48);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(121, 17);
+            this.label3.Size = new System.Drawing.Size(126, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "&Recurring Pattern:";
             // 
@@ -128,9 +128,9 @@ namespace NCrontabViewer
             // 
             this._moreButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this._moreButton.Enabled = false;
-            this._moreButton.Location = new System.Drawing.Point(491, 48);
+            this._moreButton.Location = new System.Drawing.Point(484, 48);
             this._moreButton.Name = "_moreButton";
-            this._moreButton.Size = new System.Drawing.Size(56, 24);
+            this._moreButton.Size = new System.Drawing.Size(63, 27);
             this._moreButton.TabIndex = 7;
             this._moreButton.Text = "&More";
             this._moreButton.Click += new System.EventHandler(this.More_Click);
@@ -151,7 +151,7 @@ namespace NCrontabViewer
             this._statusBarPanel.AutoSize = System.Windows.Forms.StatusBarPanelAutoSize.Spring;
             this._statusBarPanel.Name = "_statusBarPanel";
             this._statusBarPanel.Text = "Ready";
-            this._statusBarPanel.Width = 487;
+            this._statusBarPanel.Width = 542;
             // 
             // _errorProvider
             // 
@@ -160,7 +160,8 @@ namespace NCrontabViewer
             // 
             // MainForm
             // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(6, 16);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(563, 472);
             this.Controls.Add(this._statusBar);
             this.Controls.Add(this._moreButton);
@@ -171,7 +172,7 @@ namespace NCrontabViewer
             this.Controls.Add(this._resultBox);
             this.Controls.Add(this._startTimePicker);
             this.Controls.Add(this._endTimePicker);
-            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "MainForm";
             this.Text = "Crontab Viewer";
             ((System.ComponentModel.ISupportInitialize)(this._statusBarPanel)).EndInit();
