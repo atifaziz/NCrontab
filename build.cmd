5cb7f76e505c84ffac6b04c140c05451262ba704
Atif Aziz <code@raboof.com>
2015-12-08 21:38:55 +0100

    Upgrade to VS 2015 & MS Build 14

--- build.cmd
+++ build.cmd
@@ -1,25 +1,26 @@
 @echo off
 setlocal
-pushd "%~dp0"
-call :main %*
-popd
-goto :EOF
-
-:main
-set MSBUILDEXE=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
-if not exist "%MSBUILDEXE%" (
-    echo The .NET Framework 4.0 does not appear to be installed on this 
-    echo machine, which is required to build the solution.
-    exit /b 1
-)
 for %%i in (NuGet.exe) do set nuget=%%~dpnx$PATH:i
 if "%nuget%"=="" (
     echo WARNING! NuGet executable not found in PATH so build may fail!
-    echo For more on NuGet, see http://nuget.codeplex.com
+    echo For more on NuGet, see https://github.com/nuget/home
 )
+pushd "%~dp0"
 nuget restore && call :build Debug && call :build Release
+popd
 goto :EOF
 
 :build
-"%MSBUILDEXE%" NCrontab.sln /p:Configuration=%1 /v:m %2 %3 %4 %5 %6 %7 %8 %9
+setlocal
+if "%PROCESSOR_ARCHITECTURE%"=="x86" set MSBUILD=%ProgramFiles%
+if defined ProgramFiles(x86) set MSBUILD=%ProgramFiles(x86)%
+set MSBUILD=%MSBUILD%\MSBuild\14.0\bin\msbuild
+if not exist "%MSBUILD%" (
+    echo Microsoft Build Tools 2015 does not appear to be installed on this
+    echo machine, which is required to build the solution. You can install
+    echo it from the URL below and then try building again:
+    echo https://www.microsoft.com/en-us/download/details.aspx?id=48159
+    exit /b 1
+)
+"%MSBUILD%" /p:Configuration=%1 /v:m %2 %3 %4 %5 %6 %7 %8 %9
 goto :EOF
