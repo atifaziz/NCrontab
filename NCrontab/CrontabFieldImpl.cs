6ec6d46c329434f1de76d6621f2fec313290b7b8
Atif Aziz <code@raboof.com>
2016-12-20 18:37:09 +0100

    Dispose enumerator

--- NCrontab/CrontabFieldImpl.cs
+++ NCrontab/CrontabFieldImpl.cs
@@ -214,9 +214,11 @@ namespace NCrontab
             if (commaIndex > 0)
             {
                 var result = success;
-                var token = ((IEnumerable<string>) str.Split(StringSeparatorStock.Comma)).GetEnumerator();
-                while (token.MoveNext() && result == null)
-                    result = InternalParse(token.Current, acc, success, errorSelector);
+                using (var token = ((IEnumerable<string>) str.Split(StringSeparatorStock.Comma)).GetEnumerator())
+                {
+                    while (token.MoveNext() && result == null)
+                        result = InternalParse(token.Current, acc, success, errorSelector);
+                }
                 return result;
             }
 
