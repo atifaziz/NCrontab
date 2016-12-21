b88f36ee37616199ca4ae55c3502c9fbbcc893df
Atif Aziz <code@raboof.com>
2016-12-21 09:22:43 +0100

    Address TODO points in console app

--- NCrontabConsole/Program.cs
+++ NCrontabConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿#region License and Terms
+#region License and Terms
 //
 // NCrontab - Crontab for .NET
 // Copyright (c) 2008 Atif Aziz. All rights reserved.
@@ -22,7 +22,7 @@ namespace NCrontabConsole
     #region Imports
 
     using System;
-    using System.Diagnostics;
+    using System.Collections.Generic;
     using System.Globalization;
     using NCrontab;
 
@@ -32,15 +32,23 @@ namespace NCrontabConsole
     {
         static int Main(string[] args)
         {
+            var verbose = false;
+
             try
             {
-                if (args.Length < 3)
-                    throw new /* TODO Application*/Exception("Missing required arguments. You must at least supply CRONTAB-EXPRESSION START-DATE END-DATE.");
+                var argList = new List<string>(args);
+                var verboseIndex = argList.IndexOf("--verbose");
+                // ReSharper disable once AssignmentInConditionalExpression
+                if (verbose = verboseIndex >= 0)
+                    argList.RemoveAt(verboseIndex);
 
-                var expression = args[0].Trim();
-                var start = ParseDateArgument(args[1], "start");
-                var end = ParseDateArgument(args[2], "end");
-                var format = args.Length > 3 ? args[3] : "f";
+                if (argList.Count < 3)
+                    throw new ApplicationException("Missing required arguments. You must at least supply CRONTAB-EXPRESSION START-DATE END-DATE.");
+
+                var expression = argList[0].Trim();
+                var start = ParseDateArgument(argList[1], "start");
+                var end = ParseDateArgument(argList[2], "end");
+                var format = argList.Count > 3 ? argList[3] : "f";
                 var schedule = CrontabSchedule.Parse(expression, new CrontabSchedule.ParseOptions
                 {
                     IncludingSeconds = expression.Split(' ').Length > 5,
@@ -53,8 +61,12 @@ namespace NCrontabConsole
             }
             catch (Exception e)
             {
-                Console.Error.WriteLine(e.Message);
-                // TODO Trace.WriteLine(e.ToString());
+                var error =
+                    verbose
+                    ? e.ToString()
+                    : e is ApplicationException
+                    ? e.Message : e.GetBaseException().Message;
+                Console.Error.WriteLine(error);
                 return 1;
             }
         }
@@ -67,8 +79,15 @@ namespace NCrontabConsole
             }
             catch (FormatException e)
             {
-                throw new /* TODO Application*/Exception("Invalid " + hint + " date or date format argument.", e);
+                throw new ApplicationException("Invalid " + hint + " date or date format argument.", e);
             }
+        }
+
+        sealed class ApplicationException : Exception
+        {
+            public ApplicationException() {}
+            public ApplicationException(string message) : base(message) {}
+            public ApplicationException(string message, Exception inner) : base(message, inner) {}
         }
     }
 }
