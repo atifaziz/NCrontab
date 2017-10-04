f2bb7ba9bad59005344ee30612a3a9203f869268
Atif Aziz <code@raboof.com>
2017-10-04 08:02:12 +0200

    Revert non-code changes from 6b86db2
    6b86db2 = 6b86db276845f2b96b1674b5420cca59b9590837

--- NCrontab/CrontabSchedule.cs
+++ NCrontab/CrontabSchedule.cs
@@ -130,9 +130,9 @@ namespace NCrontab
             var offset = includingSeconds ? 0 : 1;
             for (var i = 0; i < tokens.Length; i++)
             {
-                var kind = (CrontabFieldKind)i + offset;
-                var field = CrontabField.TryParse(kind, tokens[i], v => new { ErrorProvider = (ExceptionProvider)null, Value = v },
-                                                                   e => new { ErrorProvider = e, Value = (CrontabField)null });
+                var kind = (CrontabFieldKind) i + offset;
+                var field = CrontabField.TryParse(kind, tokens[i], v => new { ErrorProvider = (ExceptionProvider) null, Value = v },
+                                                                   e => new { ErrorProvider = e, Value = (CrontabField) null });
                 if (field.ErrorProvider != null)
                     return errorSelector(field.ErrorProvider);
                 fields[i + offset] = field.Value;
@@ -364,7 +364,7 @@ namespace NCrontab
             // Day of week
             //
 
-            if (_daysOfWeek.Contains((int)nextTime.DayOfWeek))
+            if (_daysOfWeek.Contains((int) nextTime.DayOfWeek))
                 return nextTime;
 
             return TryGetNextOccurrence(new DateTime(year, month, day, 23, 59, 59, 0, baseTime.Kind), endTime);
