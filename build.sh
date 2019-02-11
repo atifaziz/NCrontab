a8d6399bd21920fd8bb030d481c80c8806d03934
Atif Aziz <code@raboof.com>
2019-02-11 17:07:05 +0100

    Update bash scripts to auto-CD

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-[[ -e build.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
 which dotnet 2>/dev/null || {
     echo>&2 .NET Core does not appear to be installed on this machine, which is
     echo>&2 required to build the solution. You can install it from the URL below
@@ -7,6 +6,7 @@ which dotnet 2>/dev/null || {
     echo>&2 https://dot.net
     exit 1
 }
+cd "$(dirname "$0")"
 dotnet restore \
 && for p in NCrontab NCrontab.Signed NCrontab.Tests NCrontabConsole;
     do for c in Debug Release; do {
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-[[ -e pack.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
+cd "$(dirname "$0")"
 VERSION_SUFFIX=
 if [ ! -z "$1" ]; then VERSION_SUFFIX="--version-suffix $1"; fi
 ./build.sh \
