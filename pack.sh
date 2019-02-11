ab5415797fffceb9a9d795c6e06050618c4bc66c
Atif Aziz <code@raboof.com>
2019-02-11 17:55:38 +0100

    Fix project name in pack.sh

--- pack.sh
+++ pack.sh
@@ -4,4 +4,6 @@ cd "$(dirname "$0")"
 VERSION_SUFFIX=
 if [ ! -z "$1" ]; then VERSION_SUFFIX="--version-suffix $1"; fi
 ./build.sh
-dotnet pack -c Release $VERSION_SUFFIX A1
+for p in NCrontab NCrontab.Signed; do
+    dotnet pack -c Release $VERSION_SUFFIX $p
+done
