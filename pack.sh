6bc4999dfb7689377349a6c758f9c226c062f44c
Atif Aziz <code@raboof.com>
2019-02-11 17:59:44 +0100

    Support packing on Windows only

--- pack.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-VERSION_SUFFIX=
-if [ ! -z "$1" ]; then VERSION_SUFFIX="--version-suffix $1"; fi
-./build.sh
-for p in NCrontab NCrontab.Signed; do
-    dotnet pack --no-restore --no-build -c Release $VERSION_SUFFIX $p
-done
