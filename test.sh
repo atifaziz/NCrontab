c6b44363058ddf6b40bc6d27101eb70698f6c93d
Atif Aziz <code@raboof.com>
2023-06-13 18:45:45 +0200

    Add test coverage collection & reporting

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
+dotnet tool restore
 ./build.sh
-for f in net7.0 net6.0; do {
-    dotnet test --no-build NCrontab.Tests -c Debug -f $f \
-        -p:CollectCoverage=true \
-        -p:CoverletOutputFormat=opencover \
-        -p:Exclude=[NUnit*]*
-    dotnet test --no-build NCrontab.Tests -c Release -f $f
-}
+for f in net7.0 net6.0; do
+    for c in Debug Release; do
+        dotnet test --no-build -c $c -f $f \
+            -s NCrontab.Tests/.runsettings
+    done
 done
+dotnet reportgenerator '-reports:NCrontab.Tests/TestResults/*/coverage.cobertura.xml' \
+    -targetdir:etc/coverage \
+    '-reporttypes:TextSummary;Html'
+cat etc/coverage/Summary.txt
