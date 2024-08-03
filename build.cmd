19d8509e78dced655c4d7450e5736713c9c2af88
Atif Aziz <code@raboof.com>
2024-08-03 14:02:03 +0200

    Restore build script for Windows

--- build.cmd
+++ build.cmd
@@ -5,18 +5,7 @@ popd
 goto :EOF
 
 :main
-setlocal
-set DOTNETEXE=
-for %%f in (dotnet.exe) do set DOTNETEXE=%%~dpnx$PATH:f
-if not defined DOTNETEXE set DOTNETEXE=%ProgramFiles%\dotnet
-if not exist "%DOTNETEXE%" (
-    echo .NET Core does not appear to be installed on this machine, which is
-    echo required to build the solution. You can install it from the URL below
-    echo and then try building again:
-    echo https://dot.net
-    exit /b 1
-)
-dotnet restore ^
-  && "%DOTNETEXE%" build --no-restore -c Debug ^
-  && "%DOTNETEXE%" build --no-restore -c Release
+    dotnet restore ^
+ && dotnet build --no-restore -c Debug ^
+ && dotnet build --no-restore -c Release
 goto :EOF
